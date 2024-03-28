b2b3e6a081808bb78e36d2cb55eea1007773469b
jimmy <devJimmy990@gmail.com>
2024-03-28 23:18:49 +0200

    enhancements

--- src/components/footer/footer.component.ts
+++ src/components/footer/footer.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CategoryService } from '../../services/category.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HttpClientModule],
+  providers:[CategoryService],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  Categories:any;
+  constructor(private service:CategoryService){}
+  ngOnInit(): void {
+    this.service.getCategory().subscribe({next:(data)=>this.Categories = data});
+  }
 
 }
