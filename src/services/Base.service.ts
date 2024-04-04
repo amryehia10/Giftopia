8ff0f1fe9131239b9397a5ad708d660c9ce9d284
jimmy <devJimmy990@gmail.com>
2024-04-04 05:35:41 +0200

    edit

--- src/services/Base.service.ts
+++ src/services/Base.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 
 export abstract class BaseService {
+    protected url = "";
     protected BASE_URL = 'http://localhost:7050';
     constructor(protected http: HttpClient) { }
 }
