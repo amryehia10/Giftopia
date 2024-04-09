dd2b3652dcf06a1963b294335e0766d5d1549133
ARahmaan96 <a.rahman.khallaf@gmail.com>
2024-04-09 18:12:45 +0200

    fixing product review user image.

--- src/components/product-reviews/product-reviews.component.ts
+++ src/components/product-reviews/product-reviews.component.ts
@@ -4,50 +4,56 @@ import { ReviewService } from '../../services/review.service';
 import { UserService } from '../../services/user.service';
 import { GeneralMethods } from '../../functions';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { apiEndpoint } from '../../config.json';
 
 @Component({
   selector: 'app-product-reviews',
   standalone: true,
-  imports: [CommonModule,RouterModule],
-  providers: [ReviewService,UserService],
+  imports: [CommonModule, RouterModule],
+  providers: [ReviewService, UserService],
   templateUrl: './product-reviews.component.html',
-  styleUrl: './product-reviews.component.css'
+  styleUrl: './product-reviews.component.css',
 })
-export class ProductReviewsComponent implements OnInit{
+export class ProductReviewsComponent implements OnInit {
+  apiEndpoint = apiEndpoint;
   reviews: ReviewModel[] = [];
-  users: UserModel[] = [];
-  userData: UserModel = {
-    _id : "",
-    name: "",
-    image: "",
-    address: [""],
-    email: "",
+  users: (UserModel | undefined)[] = [];
+  userData?: UserModel = {
+    _id: '',
+    name: '',
+    image: '',
+    address: [''],
+    email: '',
     age: 0,
-    password: "",
-    phone: [""],
-    gender: "",
-    userType: ""
+    password: '',
+    phone: [''],
+    gender: '',
+    userType: '',
   };
-  
-  constructor(private reviewService: ReviewService, private  userService: UserService, private route: ActivatedRoute) {}
-  
+
+  constructor(
+    private reviewService: ReviewService,
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {}
+
   ngOnInit(): void {
     const prdID = String(this.route.snapshot.paramMap.get('id'));
     this.reviewService.getReviewsByProductId(prdID).subscribe({
       next: (data) => {
         this.reviews = GeneralMethods.CastReviews(data);
-        if(this.reviews !== undefined){
+        if (this.reviews !== undefined) {
           this.reviews.forEach(async (review, index) => {
             this.userService.getUserByID(review.userId).subscribe({
               next: async (data) => {
-                this.userData =  GeneralMethods.CastUser(data);
+                this.userData = GeneralMethods.CastUser(data);
                 this.users.push(this.userData);
-              }
+              },
             });
           });
         }
       },
-      error: (err) => console.log(err)
+      error: (err) => console.log(err),
     });
   }
 }
